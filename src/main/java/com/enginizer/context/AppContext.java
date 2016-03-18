8d1de0cc8cd68d1b7e3b0729ed3dbbd8fe553fa8
dragos.triteanu <dragos.triteanu@gmail.com>
2016-03-19 00:11:31 +0100

    Innitial commit of the enginizer-boot project

---
+++ src/main/java/com/enginizer/context/AppContext.java
@@ -0,0 +1,22 @@
+package com.enginizer.context;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.enginizer.*")
+public class AppContext extends WebMvcConfigurerAdapter {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"};
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+}
---
+++ src/main/resources/views/welcome.ftl
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="css/some.css">
+</head>
+<body>
+	Date: ${time?date}
+	<br>
+	Time: ${time?time}
+	<br>
+	Message: ${message}
+</body>
+
+</html>
